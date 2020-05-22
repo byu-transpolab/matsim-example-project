cace1782310ca5adb59525622a83ea086af7d775
Greg Macfarlane <gregmacfarlane@gmail.com>
2020-05-22 15:46:22 -0600

    Add stub class files for reading ActivitySim files

---
+++ src/main/java/byu/edu/activitysimutils/ActivitySimPersonsReader.java
@@ -0,0 +1,7 @@
+package byu.edu.activitysimutils;
+
+import org.apache.log4j.Logger;
+
+public class ActivitySimPersonsReader {
+    private static final Logger log = Logger.getLogger(ActivitySimPersonsReader.class);
+}
---
+++ src/main/java/byu/edu/activitysimutils/ActivitySimTripsReader.java
@@ -0,0 +1,7 @@
+package byu.edu.activitysimutils;
+
+import org.apache.log4j.Logger;
+
+public class ActivitySimTripsReader {
+    private static final Logger log = Logger.getLogger(ActivitySimTripsReader.class);
+}
