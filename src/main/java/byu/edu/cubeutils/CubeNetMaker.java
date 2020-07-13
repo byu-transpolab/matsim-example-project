46306ba5af45e7d7637cff020936b303a254571f
Greg Macfarlane <gregmacfarlane@gmail.com>
2020-07-13 14:29:03 -0600

    Add CubeLine java class
    This class helps us stash and organize the data coming in from the line files.

---
+++ src/main/java/byu/edu/cubeutils/CubeNetMaker.java
@@ -0,0 +1,5 @@
+package byu.edu.cubeutils;
+
+public class CubeNetMaker {
+    private static final Logger log = Logger.getLogger(CubeNetMaker.class);
+}
---
+++ src/main/java/byu/edu/cubeutils/CubeTransitReader.java
@@ -0,0 +1,5 @@
+package byu.edu.cubeutils;
+
+public class CubeTransitReader {
+    private static final Logger log = Logger.getLogger(CubeTransitReader.class);
+}
