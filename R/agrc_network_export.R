aba48127a3f6099c01a3cc8770109b3ddd495e63
Greg Macfarlane <gregmacfarlane@byu.edu>
2020-08-07 15:49:40 -0600

    Update to use multimodal network

--- R/agrc_network_export.R
+++ R/agrc_network_export.R
@@ -6,18 +6,18 @@ library(tidyverse)
 
 
 # Get AGRC Network data ==========
-# The Utah AGRC street network database is available at 
-#   https://gis.utah.gov/data/transportation/street-network-analysis/
+# The Utah AGRC multimodal network database is available at 
+#   https://gis.utah.gov/data/transportation/street-network-analysis/#MultimodalNetwork
 #   
 # For this research we downloaded the information from that file in August 2020.
 # The file we downloaded is available on Box, but is not committed to the 
 # repository for space reasons. This file contains code to download the archived 
 # AGRC network, extract it. 
-filegdb <- "data/UtahRoadsNetworkAnalysis.gdb"
+filegdb <- "data/MM_NetworkDataset_07272020.gdb"
 if(!file.exists(filegdb)) {
   zippedgdb <- "data/agrc_network.zip"
   if(!file.exists(zippedgdb)) {
-    download.file("https://byu.box.com/shared/static/dw7jhe5s7pwyr0tbzodi4tcyo41zafxa.zip",
+    download.file("https://byu.box.com/shared/static/qyjf1of9dau7tyc3rptxc9w4fi08x3d6.zip",
       zippedgdb)
   }
   system2("7z", c("e", zippedgdb, str_c("-o", filegdb)) )
