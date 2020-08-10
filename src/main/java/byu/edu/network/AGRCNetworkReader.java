0551599080e3f4116d54e8c6e17f4cedea1a7fcf
Greg Macfarlane <gregmacfarlane@gmail.com>
2020-08-10 14:11:20 -0600

    Handle network link information

--- src/main/java/byu/edu/network/AGRCNetworkReader.java
+++ src/main/java/byu/edu/network/AGRCNetworkReader.java
@@ -6,6 +6,7 @@ import org.apache.log4j.Logger;
 import org.matsim.api.core.v01.Coord;
 import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.network.Link;
 import org.matsim.api.core.v01.network.Network;
 import org.matsim.api.core.v01.network.NetworkFactory;
 import org.matsim.api.core.v01.network.Node;
@@ -85,7 +86,73 @@ public class AGRCNetworkReader {
     }
 
 
-    private void readLinks(File linksFile){
+    /**
+     * Read the network links information
+     * @param linksFile A csv file containing the following fields:
+     *                  - link_id,
+     *                  - Oneway,
+     *                  - DriveTime (minutes)
+     *                  - Length (miles)
+     *                  - RoadClass
+     *                  - AADT (count)
+     *                  - start_node
+     *                  - end_node
+     *                  - ft
+     *                  - lanes
+     *                  - sl (miles per hour)
+     *                  - med median treatment
+     *                  - terrain
+     *                  - capacity (vehicles / hr)
+     */
+    private void readLinks(File linksFile) throws IOException {
+        CSVReader reader = CSVUtils.createCSVReader(linksFile.toString());
+        String[] header = reader.readNext();
+        Map<String, Integer> col = CSVUtils.getIndices(header,
+                new String[]{"link_id", "Oneway", "start_node", "end_node", "Length_Miles",
+                        "capacity", "DriveTime", "lanes"}, // mandatory columns
+                new String[]{"Speed", "AADT", "RoadClass"} // optional columns
+        );
+
+        String[] nextLine;
+        while ((nextLine = reader.readNext()) != null){
+            // set up link ID with from and to nodes
+            Id<Node> fromNodeId = Id.createNodeId(nextLine[col.get("start_node")]);
+            Id<Node> toNodeId   = Id.createNodeId(nextLine[col.get("end_node")]);
+            Node fromNode = network.getNodes().get(fromNodeId);
+            Node toNode   = network.getNodes().get(toNodeId);
+            Id<Link> linkId = Id.createLinkId(nextLine[col.get("link_id")]);
+            Link l = networkFactory.createLink(linkId, fromNode, toNode);
+
+            // get link attributes from csv
+            Double driveTime = Double.valueOf(nextLine[col.get("DriveTime")]);
+            Double lengthMiles = Double.valueOf(nextLine[col.get("Length_Miles")]);
+            Double capacity = Double.valueOf(nextLine[col.get("capacity")]);
+            Integer lanes = Integer.valueOf(nextLine[col.get("lanes")]);
+            Integer oneWay = Integer.valueOf(nextLine[col.get("Oneway")]);
+
+            Double length = lengthMiles * 1609.34; // convert miles to meters
+            Double freeSpeed = length / (driveTime * 60); // convert meters per minute to meters per second
+
+            // put link attributes on link
+            l.setLength(length);
+            l.setFreespeed(freeSpeed);
+            l.setNumberOfLanes(lanes);
+            l.setCapacity(capacity);
+            network.addLink(l);
+
+            // create reverse direction link if it exists
+            if(oneWay != 1) {
+                Id<Link> rLinkId = Id.createLinkId(nextLine[col.get("link_id")] + "r");
+                Link rl = networkFactory.createLink(rLinkId, toNode, fromNode);
+
+                rl.setLength(length);
+                rl.setFreespeed(freeSpeed);
+                rl.setNumberOfLanes(lanes);
+                rl.setCapacity(capacity);
+                network.addLink(rl);
+            }
+
+        }
 
     }
 
